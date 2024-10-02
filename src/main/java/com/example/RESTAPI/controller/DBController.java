add70aaab8352fff0d542fcd6b92bd9748048c94
Artem Valeev <acc4dev404@gmail.com>
2024-10-03 00:57:43 +0300

    Add Change

--- src/main/java/com/example/RESTAPI/controller/DBController.java
+++ src/main/java/com/example/RESTAPI/controller/DBController.java
@@ -1,61 +1,71 @@
 package com.example.RESTAPI.controller;
 
 import com.example.RESTAPI.model.User;
+import org.springframework.stereotype.Component;
 
 import java.sql.*;
 
+@Component
 public class DBController {
 
-    private final String url;
-    private final String user;
-    private final String password;
+    private final String url_conn = "jdbc:postgresql://192.168.1.45:5432/loadDB";
+    private final String user_conn = "loaderDB";
+    private final String password_conn = "ReSo999+";
 
-    public DBController(String url, String user, String password) {
-        this.url = url;
-        this.user = user;
-        this.password = password;
+    public DBController() {
     }
 
-    public User getUser(String login) {
+    public User getUser(String login) throws Exception {
+        User user = null;
+        Connection conn = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+        String sqlQuery = String.format("SELECT users.*, emails.email FROM users INNER JOIN emails ON users.login = emails.login WHERE users.login LIKE '%s';", login);
         try {
-            Connection conn = DriverManager.getConnection(url, user, password);
-            Statement stmt = conn.createStatement();
-            String sqlQuery = String.format("SELECT users.*, emails.email FROM users INNER JOIN emails ON users.login = emails.login WHERE users.login LIKE '%s';", login);
-            ResultSet rs = stmt.executeQuery(sqlQuery);
+            conn = DriverManager.getConnection(url_conn, user_conn, password_conn);
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(sqlQuery);
             if (rs.next()) {
-                return new User(login, rs.getString("password"), rs.getTimestamp("date").toLocalDateTime(), rs.getString("email"));
-            }
-            rs.close();
-            stmt.close();
-            conn.close();
+                user = new User(login, rs.getString("password"), rs.getTimestamp("date").toLocalDateTime(), rs.getString("email"));
+            } else
+                throw new Exception("User not found");
         } catch (SQLException e) {
             System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
+            throw new RuntimeException(e);
         } catch (Exception e) {
-            e.printStackTrace();
+            //e.printStackTrace();
             System.err.format("ERROR: %s\n", e.getMessage());
+            throw new RuntimeException(e);
+        } finally {
+            if (rs != null)
+                rs.close();
+            if (stmt != null)
+                stmt.close();
+            if (conn != null)
+                conn.close();
         }
-        return null;
+        return user;
     }
 
     public int createUser(User user) {
         int rows = 0;
-        try (Connection connection = DriverManager.getConnection(
-                "jdbc:postgresql://192.168.1.45:5432/loadDB", "loaderDB", "ReSo999+")) {
-            String sqlQuery = "INSERT INTO users (login, password, date) VALUES (?, ?, ?); \n INSERT INTO emails (login, email) VALUES (?, ?);";
-            try (PreparedStatement pstmt = connection.prepareStatement(sqlQuery)) {
-                pstmt.setString(1, user.getLogin());
-                pstmt.setString(2, user.getPassword());
-                pstmt.setTimestamp(3, Timestamp.valueOf(user.getDate()));
-                pstmt.setString(4, user.getLogin());
-                pstmt.setString(5, user.getEmail());
-                rows = pstmt.executeUpdate();
-            } catch (SQLException e) {
-                e.printStackTrace();
-                System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
-            }
+        String sqlQuery = "INSERT INTO users (login, password, date) VALUES (?, ?, ?); \n INSERT INTO emails (login, email) VALUES (?, ?);";
+        try (Connection connection = DriverManager.getConnection(url_conn, user_conn, password_conn);
+             PreparedStatement pstmt = connection.prepareStatement(sqlQuery)) {
+            pstmt.setString(1, user.getLogin());
+            pstmt.setString(2, user.getPassword());
+            pstmt.setTimestamp(3, Timestamp.valueOf(user.getDate()));
+            pstmt.setString(4, user.getLogin());
+            pstmt.setString(5, user.getEmail());
+            rows = pstmt.executeUpdate();
+            if (rows == 0)
+                throw new Exception("User has not been created");
+        } catch (SQLException e) {
+            System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
+            throw new RuntimeException(e);
         } catch (Exception e) {
-            e.printStackTrace();
             System.err.format("ERROR: %s\n", e.getMessage());
+            throw new RuntimeException(e);
         }
         return rows;
     }
--- src/main/java/com/example/RESTAPI/controller/MainController.java
+++ src/main/java/com/example/RESTAPI/controller/MainController.java
@@ -3,6 +3,7 @@ package com.example.RESTAPI.controller;
 import com.example.RESTAPI.model.Message;
 
 import com.example.RESTAPI.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +17,9 @@ import java.util.Map;
 @RestController
 public class MainController {
 
+    @Autowired
+    DBController db_connect;
+
     private void setOffsetTime(int minOffsetTime, int maxOffsetTime) throws InterruptedException {
         Thread.sleep( (int) ((Math.random() * (maxOffsetTime - minOffsetTime)) + minOffsetTime) );
     }
@@ -24,16 +28,12 @@ public class MainController {
     @ResponseBody
     public ResponseEntity<?> getUserByLogin(@RequestParam String login) throws InterruptedException {
         setOffsetTime(1000,2000);
+        User user = null;
         try {
-            DBController db_connect = new DBController("jdbc:postgresql://192.168.1.45:5432/loadDB", "loaderDB", "ReSo999+");
-            User user;
-            if ((user = db_connect.getUser(login)) == null)
-                throw new Exception("Login not found");
+            user = db_connect.getUser(login);
             return new ResponseEntity<>(user, HttpStatus.OK);
         } catch (Exception e) {
-            e.printStackTrace();
-            System.err.format("ERROR: %s\n", e.getMessage());
-            return new ResponseEntity<>(new Message(e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
@@ -42,23 +42,15 @@ public class MainController {
     public ResponseEntity<?> postUsers(@RequestBody Map<String, String> body) throws InterruptedException {
         setOffsetTime(1000,2000);
         try {
-            if (
-                body.size() == 3 &&
+            if (body.size() == 3 &&
                 body.get("login") != null && body.get("password") != null && body.get("email") != null &&
-                !body.get("login").trim().isEmpty() && !body.get("password").trim().isEmpty() && !body.get("email").trim().isEmpty()
-            )
-            {
-                DBController db_connect = new DBController("jdbc:postgresql://192.168.1.45:5432/loadDB", "loaderDB", "ReSo999+");
-                if (db_connect.createUser(new User(body.get("login"), body.get("password"), body.get("email"))) == 0) {
-                    throw new Exception("Login already exists or the user data is incorrect");
-                }
-                return new ResponseEntity<>(new Message("User created") , HttpStatus.OK);
-            }
-            throw new Exception("JSON is incorrect");
+                !body.get("login").trim().isEmpty() && !body.get("password").trim().isEmpty() && !body.get("email").trim().isEmpty()) {
+                db_connect.createUser(new User(body.get("login"), body.get("password"), body.get("email")));
+                return new ResponseEntity<>(HttpStatus.OK);
+            } else
+                throw new Exception("JSON is incorrect");
         } catch (Exception e) {
-            e.printStackTrace();
-            System.err.format("ERROR: %s\n", e.getMessage());
-            return new ResponseEntity<>(new Message(e.getMessage()), HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
     }
 
